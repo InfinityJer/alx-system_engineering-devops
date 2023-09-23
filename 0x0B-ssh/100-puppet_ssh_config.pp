6daa2f5bb8be2c049207db810278fe557e2cc74d
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-09-23 22:06:55 +0800

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,24 +1,15 @@
 # Puppet manifest to configure SSH client
-
-# Ensure the SSH client configuration directory exists
-file { '/home/yourusername/.ssh':
-  ensure => 'directory',
-  owner  => 'yourusername',
-  group  => 'yourusername',
-  mode   => '0700',
+# Ensure the SSH client configuration file exists
+file_line { 'replace passwordAuthentication':
+    ensure  => 'present',
+    path    => '/etc/ssh/ssh_config',
+    replace => true,
+    line    => 'PasswordAuthentication no',
+    match   => 'PasswordAuthentication yes',
 }
 
-# Ensure the SSH client configuration file exists
-file { '/home/yourusername/.ssh/config':
-  ensure => 'file',
-  owner  => 'yourusername',
-  group  => 'yourusername',
-  mode   => '0600',
-  content => "\
-Host your-server-alias
-    HostName 98.98.98.98  # Replace with the actual IP or hostname of your server
-    User ubuntu
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no
-",
+file_line { 'add private key':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/.ssh/school',
 }
