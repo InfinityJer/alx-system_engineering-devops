2c3728d4f8568af6be2e1c374b7e5e9a13944a51
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-09-27 22:34:51 +0800

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,4 +1,4 @@
-# Install Nginx package
+# Install Nginx package with puppet 
 package { 'nginx':
   ensure => 'installed',
 }
@@ -10,30 +10,11 @@ file { '/var/www/html/index.html':
   require => Package['nginx'],
 }
 
-# Configure Nginx to listen on port 80
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  source  => 'puppet:///modules/nginx/default',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+exec { 'append_redirect_me':
+  command => "/usr/bin/sed -i '/^}$/i \\\n\tlocation \\/redirect_me {return 301 https:\\/\\/www.youtube.com\\/watch?v=QH2-TGUlwu4;}' /etc/nginx/sites-available/default",
 }
 
-# Enable the default site (symlink)
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => 'link',
-  target  => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-  notify  => Service['nginx'],
-}
-
-# Define the custom 301 redirect
-nginx::resource::vhost { 'redirect_me':
-  www_root    => '/var/www/html',
-  listen_port => 80,
-  server_name => 'localhost',
-  location    => {
-    '/' => {
-      rewrite => '^/redirect_me / https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-    },
-  },
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
