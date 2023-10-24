bd6d1f5cba58537fabbe78f16d04390f99ef16bc
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-10-25 00:08:22 +0800

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,32 +2,34 @@
 """
 Gather data from an API
 """
+import json
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
 
-    employee_id = sys.argv[1]
-    base_url = 'https://jsonplaceholder.typicode.com'
+    sessionReq = requests.Session()
 
-    user_response = requests.get(f'{base_url}/users/{employee_id}')
-    todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
+    idEmp = argv[1]
+    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
 
-    try:
-        employee_name = user_response.json()[0].get('name')
-        todos = todos_response.json()
-        total_tasks = len(todos)
-        done_tasks = [task for task in todos if task.get('completed')]
-        num_done_tasks = len(done_tasks)
+    employee = sessionReq.get(idURL)
+    employeeName = sessionReq.get(nameURL)
 
-        print(f"Employee {employee_name} is done with tasks "
-              f"({num_done_tasks}/{total_tasks}):")
-        for task in done_tasks:
-            print("\t " + task.get('title'))
+    json_req = employee.json()
+    name = employeeName.json()['name']
 
-    except Exception as e:
-        print(f"Error: {e}")
+    totalTasks = 0
+
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            totalTasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, totalTasks, len(json_req)))
+
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            print("\t " + done_tasks.get('title'))
